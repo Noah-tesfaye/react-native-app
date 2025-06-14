8af4ac13a203fc6a6579b9201f98dc66200aea4b
Noah-tesfaye <73971339+Noah-tesfaye@users.noreply.github.com>
2025-06-14 03:37:23 +0000

    added a simple counter and tested it

--- MyFirstApp/App.tsx
+++ MyFirstApp/App.tsx
@@ -1,10 +1,18 @@
+import {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
+  const[count,setCount]=useState(0);
   return (
     <View style={styles.container}>
-      <Text>This is my first react native application</Text>
+      <Text style={styles.text}>This is my first react native application</Text>
+      <Text style={styles.counter}>{count}</Text>
+      <Button
+      title="This is a self closing button please click me"
+      color="green"
+      onPress={() => setCount(count+10)}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +25,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 24,
+    color: 'blue',
+    margin: 10,
+  },
+    counter:{
+    fontSize: 14,
+    color: 'red',
+    margin: 10,
+  }
 });
